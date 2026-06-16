01af5d0e7dc46fc4c366ee2a093405cd452ff5dd
Eugene Mah <eugenemah@gmail.com>
2026-06-16 09:33:27 -0400

    Model files

---
+++ app/Models/Kfactor.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Kfactor extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * Fillable attributes
+     * 
+     * @var array<string>
+     */
+    protected $fillable = [
+        'scanner_id',
+        'manufacturer_id',
+        'phantom_diameter',
+        'shaped_filter',
+        'kv',
+        'spectral_filter',
+        'coll_N',
+        'coll_T',
+        'coll_width',
+        'ctdi100_center',
+        'ctdi_w'
+    ];
+
+    
+    /**
+     * Attribute casting
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at'   => 'datetime',
+            'deleted_at'   => 'datetime',
+            'updated_at'   => 'datetime',
+        ];
+    }
+
+    /**
+     * Relationships
+     */
+    public function manufacturer(): BelongsTo
+    {
+        return $this->belongsTo(Manufacturer::class);
+    }
+
+    public function scanner(): BelongsTo
+    {
+        return $this->belongsTo(Scanner::class);
+    }
+}
---
+++ app/Models/Scanner.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+#[Fillable(['model'])]
+class Scanner extends Model
+{
+    use SoftDeletes;
+    
+    /**
+     * Attribute casting
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at'   => 'datetime',
+            'deleted_at'   => 'datetime',
+            'updated_at'   => 'datetime',
+        ];
+    }
+
+    /**
+     * Relationships
+     */
+    public function manufacturer(): BelongsTo
+    {
+        return $this->belongsTo(Manufacturer::class);
+    }
+
+    public function kFactor(): HasMany
+    {
+        return $this->hasMany(Kfactor::class);
+    }
+}
