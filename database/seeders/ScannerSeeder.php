63d8ca5e7f898ffe83d9a2d7c3df8f227fb449e5
Eugene Mah <eugenemah@gmail.com>
2026-06-16 10:07:38 -0400

    DB seeders

---
+++ database/seeders/ScannerSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ScannerSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Scanners to seed the DB with
+     * 
+     * @var array $scanners
+     */
+    protected $scanners = [
+        // Siemens
+        1 => [
+            'Naeotom Alpha',
+            'Definition AS/AS+',
+            'Somatom Flash',
+            'Somatom Force',
+            'Somatom Drive',
+            'Definition Edge',
+            'go.Top',
+            'Confidence 64',
+            'Confidence 20',
+            'X.cite',
+            'Biograph Vision',
+            'Intevo Bold',
+            'Symbia Pro.specta',
+            'Pro.Pulse',
+        ],
+        2 => [
+            // Neurologica
+            'BodyTom',
+        ],
+    ];
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach ($this->scanners as $manuf => $scanner) {
+            foreach ($scanner as $s) {
+                DB::table('scanners')->insert(
+                    [
+                        'manufacturer_id' => $manuf,
+                        'model' => $s,
+                    ]
+                );
+            }
+        }
+    }
+}
