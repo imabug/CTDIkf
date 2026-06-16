ce46656a99c4667ca52980af268de0c2fd14caca
Eugene Mah <eugenemah@gmail.com>
2026-06-16 13:42:02 -0400

    Seeder for k factors

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ManufacturerSeeder::class,
             ScannerSeeder::class,
+            KFactorSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/KFactorSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Kfactor;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use SplFileObject;
+
+use function Laravel\Prompts\info;
+
+class KFactorSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the kfactors table from a CSV file
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        $csv = new SplFileObject('database/data/kfactors.csv', 'r');
+        $csv->setFlags(SplFileObject::READ_CSV);
+        $csv->seek(1); // Skip the header line
+
+        while ($csv->valid()) {
+            $row = $csv->current();
+            if (is_null($row[0])) {
+                break;
+            }
+            Kfactor::create([
+                'scanner_id' => $row[1],
+                'manufacturer_id' => $row[2],
+                'phantom_diameter' => $row[3],
+                'shaped_filter' => $row[4],
+                'kv' => $row[5],
+                'spectral_filter' => $row[6],
+                'coll_N' => $row[7],
+                'coll_T' => $row[8],
+                'coll_width' => $row[9],
+                'ctdi100_center' => $row[10],
+                'ctdi_w' => $row[11],
+            ]);  
+            $csv->next();
+        }
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+}
