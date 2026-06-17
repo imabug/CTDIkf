f60023d12ed38b44f80949c50af60b7c9516da31
Eugene Mah <eugenemah@gmail.com>
2026-06-17 14:33:53 -0400

    Work on table filtering and grouping

--- app/Filament/Resources/Kfactors/Tables/KfactorsTable.php
+++ app/Filament/Resources/Kfactors/Tables/KfactorsTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Kfactors\Tables;
 
+use App\Models\Kfactor;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
@@ -9,62 +10,142 @@ use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Grouping\Group;
 use Filament\Tables\Table;
+use Illuminate\Support\Facades\DB;
 
 class KfactorsTable
 {
+    private function scanners(): array
+    {
+        /**
+         * Return an array of scanners to be used in filter options
+         */
+
+        return [];
+    }
+
+    private function manufacturers(): array
+    {
+        /**
+         * return an array of manufacturers to be used in filter options
+         */
+        return [];
+    }
+
+    private function nt(): array
+    {
+        /**
+         * Return an array of collimated widths to be used in filter options
+         */
+        return [];
+    }
+
+    private function kv(): array
+    {
+        /**
+         * Return an array of tube voltages to be used in filter options
+         */
+        return [];
+    }
+
+    private function width(): array
+    {
+        /**
+         * Return an array of collimated widths to be used in filter options
+         */
+        return [];
+    }
+
     public static function configure(Table $table): Table
     {
         return $table
             ->columns([
-                TextColumn::make('scanner.id')
-                    ->searchable(),
-                TextColumn::make('manufacturer.id')
-                    ->searchable(),
+                TextColumn::make('scanner')
+                    ->searchable()
+                    ->visible(function ($livewire) {
+                        $filterState = $livewire->getTableFilterState('scanner');
+                        if ($filterState == null
+                        || (is_array($filterState) && $filterState['value'] == "")
+                        ) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }),
+                TextColumn::make('manufacturer')
+                    ->searchable()
+                    ->visible(function ($livewire) {
+                        $filterState = $livewire->getTableFilterState('scanner');
+                        if ($filterState == null
+                        || (is_array($filterState) && $filterState['value'] == "")
+                        ) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }),
                 TextColumn::make('phantom_diameter')
+                    ->label('Phantom diameter (cm)')
                     ->numeric()
-                    ->sortable(),
+                    ->sortable()
+                    ->wrap(),
                 TextColumn::make('shaped_filter')
-                    ->searchable(),
+                    ->searchable()
+                    ->wrap(),
                 TextColumn::make('kv')
                     ->numeric()
                     ->sortable(),
                 TextColumn::make('spectral_filter')
-                    ->searchable(),
-                TextColumn::make('coll_N')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('coll_T')
-                    ->numeric()
+                    ->searchable()
+                    ->wrap(),
+                TextColumn::make('coll_NT')
+                    ->label('NxT')
                     ->sortable(),
                 TextColumn::make('coll_width')
+                    ->label('Collimation width (mm)')
                     ->numeric()
-                    ->sortable(),
+                    ->sortable()
+                    ->wrap(),
                 TextColumn::make('ctdi100_center')
+                    ->label('CTDI(100) center')
                     ->numeric()
-                    ->sortable(),
+                    ->sortable()
+                    ->wrap(),
                 TextColumn::make('ctdi_w')
+                    ->label('CTDIw')
                     ->numeric()
                     ->sortable(),
                 TextColumn::make('k_factor')
                     ->numeric()
                     ->sortable(),
-                TextColumn::make('deleted_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
             ])
+            ->groups([
+                Group::make('manufacturer')
+                    ->collapsible(),
+                Group::make('scanner')
+                    ->collapsible(),
+                Group::make('phantom_diameter')
+                    ->label('Phantom Diameter')
+                    ->collapsible(),
+            ])
+            ->defaultGroup('scanner')
             ->filters([
                 TrashedFilter::make(),
+                SelectFilter::make('scanner')
+                    ->options(Kfactor::get()->pluck('scanner')->unique())
+                    ->searchable(),
+                SelectFilter::make('phantom_diameter')
+                    ->options([
+                        '16' => '16 cm',
+                        '32' => '32 cm',
+                    ]),
+                SelectFilter::make('coll_width')
+                    ->label('Collimated width (mm)'),
+                SelectFilter::make('kv')
+                    ->label('kV'),
             ])
             ->recordActions([
                 ViewAction::make(),
